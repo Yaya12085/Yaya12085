2bd41735e3bd18318cf7c41d409b7b05708cf4e5
Yaya12085 <yayamohamed883@gmail.com>
2023-08-03 09:23:21 +0000

    updated

--- scripts/update_readme.js
+++ scripts/update_readme.js
@@ -3,7 +3,8 @@ const currentDate = new Date().toLocaleString("en-US", {
   month: "2-digit",
   year: "numeric",
   hour: "2-digit",
-  minute: "2-digit"
+  minute: "2-digit",
+  second: "2-digit"
 });
 const readmeContent = `### Hi there 👋
 
